746747e692f4dcd028fba9e86b289a074df40762
Kun Zhang <zhangkun322@qq.com>
2019-09-05 14:31:22 +0800

    Update Run_MOD16_v1.m

--- Run_MOD16_v1.m
+++ Run_MOD16_v1.m
@@ -1,4 +1,3 @@
-
 % The each day was divided into Daytime and Nighttime
 % Forcing Data:
 % -------------------------------------------------------------------------
@@ -23,39 +22,39 @@
 % -------------------------------------------------------------------------
 % Get the Soil heat flux data
 disp(' ******          Get the Soil heat flux data        ****** ')
-[G_D,G_N] = Get_Soil_G(Ta_D,Ta_N,Rn_D,Rn_N,Fc,Tmin_close,Tann);
+[G_D, G_N] = Get_Soil_G(Ta_D, Ta_N, Rn_D, Rn_N, Fc, Tmin_close, Tann);
 
 % Call MOD16
 disp('        #####------ -------------------- ------##### ')
 disp('        #####------ Call MOD16 algorithm ------##### ')
 disp('        #####------ -------------------- ------##### ')
 
-[ET_D,Ewet_D,Ttrans_D,Esoil_D] = MOD16_global(Rn_D,Ta_D,RH_D,Pa_D,Tmin_D,G_D,1,Fc,LAI,BPLUT,daycont);
-[ET_N,Ewet_N,Ttrans_N,Esoil_N] = MOD16_CMG(Rn_N,Ta_N,RH_N,Pa_N,Tmin_N,G_N,0,Fc,LAI,BPLUT,24-daycont);
+[ET_D, Ewet_D, Ttrans_D, Esoil_D] = MOD16_global(Rn_D, Ta_D, RH_D, Pa_D, Tmin_D, G_D, 1, Fc, LAI, BPLUT, daycont);
+[ET_N, Ewet_N, Ttrans_N, Esoil_N] = MOD16_CMG(Rn_N, Ta_N, RH_N, Pa_N, Tmin_N, G_N, 0, Fc, LAI, BPLUT, 24 - daycont);
 
 disp(' ****** Delete abnormal value according to MODIS Fc data ****** ')
 % Total Ewet
 Ewet = Ewet_D + Ewet_N;
-Ewet(Fc>1) = 0; Ewet = single(Ewet);
+Ewet(Fc > 1) = 0; Ewet = single(Ewet);
 % Total Ttrans
 Ttrans = Ttrans_D + Ttrans_N;
-Ttrans(Fc>1) = 0; Ttrans = single(Ttrans);
+Ttrans(Fc > 1) = 0; Ttrans = single(Ttrans);
 % Total Esoil
 Esoil = Esoil_D + Esoil_N;
-Esoil(Fc>1) = 0; Esoil = single(Esoil);
+Esoil(Fc > 1) = 0; Esoil = single(Esoil);
 % Total ET
 ET = ET_D + ET_N;
-ET(Fc>1) = 0; ET = single(ET);
+ET(Fc > 1) = 0; 
+ET = single(ET);
 
 % write to
 disp(' ****** Writting result to specific output folder ****** ')
 
-save([fileout1 '\ET.Y' year '.DOY' DOY '.mat'],'ET');
-save([fileout2 '\Ewet.Y' year '.DOY' DOY '.mat'],'Ewet');
-save([fileout3 '\Trans.Y' year '.DOY' DOY '.mat'],'Ttrans');
-save([fileout4 '\Esoil.Y' year '.DOY' DOY '.mat'],'Esoil');
+save([fileout1 '\ET.Y' year '.DOY' DOY '.mat'], 'ET');
+save([fileout2 '\Ewet.Y' year '.DOY' DOY '.mat'], 'Ewet');
+save([fileout3 '\Trans.Y' year '.DOY' DOY '.mat'], 'Ttrans');
+save([fileout4 '\Esoil.Y' year '.DOY' DOY '.mat'], 'Esoil');
 nn = nn + 1;
 disp(' ******              Writting finished            ****** ')
 disp([' =======================  DOY : ' DOY ' Finished  ======================= '])
 disp('                                 ')
-
